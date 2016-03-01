603ad5af56a5c05383dc9da6bece3c77eedc3edd
elf Pavlik <perpetual-tripper@wwelves.org>
2016-03-01 00:19:54 -0600

    further refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
 var gulp = require('gulp')
+var uuid = require('node-uuid')
 var level = require('level')
 var LevelPromise = require('level-promise')
 var forkdb = require('forkdb')
 var fdbp = require('forkdb-promise')
 var fetch = require('github-fetch')
+var Storage = require('o-storage-forkdb').default
+var Dataset = require('o-utils-dataset').default
+var SimpleRDF = require('simplerdf')
 var transform = require('./index')
 var config = require('./config.json')
 
 var cache = forkdb(level(config.db.cache.level), { dir: config.db.cache.fork })
-var lod = forkdb(level(config.db.lod.level), { dir: config.db.lod.fork })
 var mappings = level(config.db.mappings.level)
 LevelPromise(mappings)
+var dataset = new Dataset(new Storage(config.db.lod))
 
 var prefix = 'https://api.github.com/'
 var options = { token: config.api.token }
 
+// TODO add done() by using Promise.all
 gulp.task('fetch', function () {
   config.repos.map(function (repo) {
     var repoUrl = prefix + 'repos/' + repo.source
@@ -42,54 +47,80 @@ function ghIssuesUrl (repo) {
   return prefix + 'repos/' + repo.source + '/issues'
 }
 
+function gh2lod (gh, uriSpace, transformFunction) {
+  return mappings.get(gh.id)
+    .then(function (resourceUri) {
+      // TODO implement update
+      return Promise.resolve(resourceUri)
+    }, function (err) {
+      if (err.type === 'NotFoundError') {
+        var resourceUri = uriSpace + uuid.v4()
+        return transformFunction(gh, resourceUri)
+          .then(function (graph) {
+            return Promise.all([
+              dataset.createResource(resourceUri, graph),
+              mappings.put(gh.id, resourceUri)
+            ])
+          }).then(function () {
+            return Promise.resolve(resourceUri)
+          })
+      } else {
+        return Promise.reject(err)
+      }
+    })
+}
+
+/**
+ * 1. get github json from cache
+ * 2. update or create creation
+ * 3. get or create tasks container
+ * 4. update or create issues
+ * 5. update tasks container (contains)
+ */
 function processRepo (repo) {
-  var ghRepo
-  var repold
-  return fdbp.get(cache, ghRepoUrl(repo)).then(function (string) {
-    ghRepo = JSON.parse(string)
-    return mappings.get(ghRepo.id)
-  }).then(function (url) {
-      // TODO
-  }, function (err) {
-    if (err.type === 'NotFoundError') {
-      repold = transform.repo(ghRepo, repo.uriSpace)
-      var repoUri = repold['@graph'][0].id.replace('#id', '')
-      return fdbp.put(lod, repoUri, JSON.stringify(repold))
-    } else {
-      return Promise.reject(err)
-    }
-  }).then(function () {
-    return processIssues(repo)
-  }).then(function (containerMembers) {
-    var container = new transform.Resource()
-    var tasks = repold['@graph'][1] // FIXME
-    tasks['ldp:memeber'] = containerMembers
-    container['@graph'].push(tasks)
-    return fdbp.put(lod, tasks.id, JSON.stringify(container))
-  })
+  var creationUrl
+  var tasksUri
+  var link = {
+    'http://www.w3.org/ns/ldp#hasMemberRelation': 'http://www.w3.org/2005/01/wf/flow#task'
+  }
+  return fdbp.get(cache, ghRepoUrl(repo))
+    .then(function (ghRepoStr) {
+      var json = JSON.parse(ghRepoStr)
+      return gh2lod(json, repo.uriSpace, transform.repo)
+    }).then(function (url) {
+      creationUrl = url
+      return dataset.getLinkedContainerUri(creationUrl, link)
+    }).then(function (containerUri) {
+      if (containerUri) {
+        return Promise.resolve(containerUri)
+      } else {
+        containerUri = repo.uriSpace + uuid.v4()
+        return dataset.createLinkedContainer(containerUri, creationUrl, link)
+      }
+    }).then(function (containerUri) {
+      tasksUri = containerUri
+      return processIssues(repo)
+    }).then(function (memberUris) {
+      var tasks = SimpleRDF(transform.context, tasksUri)
+      tasks.contains = memberUris
+      return dataset.appendToResource(tasksUri, tasks.graph())
+    })
 }
 
 function processIssues (repo) {
-  var containerMembers = []
   return fdbp.get(cache, ghIssuesUrl(repo))
     .then(function (string) {
       var ghIssues = JSON.parse(string)
       return Promise.all(ghIssues.map(function (ghIssue) {
-        var taskld = transform.task(ghIssue, repo.uriSpace)
-        var taskUri = taskld['@graph'][0].id // FIXME
-        containerMembers.push(taskUri)
-        var resourceUri = taskUri.replace('#id', '') // FIXME
-        return fdbp.put(lod, resourceUri, JSON.stringify(taskld))
+        return gh2lod(ghIssue, repo.uriSpace, transform.issue)
       }))
-    }).then(function (hashes) {
-      return Promise.resolve(containerMembers)
     })
 }
 
 gulp.task('process', function (done) {
   Promise.all(config.repos.map(processRepo))
     .then(function (hashes) {
-      console.log(hashes)
+      console.log('processed ' + config.repos.length + ' repos')
       done()
     }).catch(function (err) {
       console.log(err)
@@ -98,4 +129,5 @@ gulp.task('process', function (done) {
 })
 
 gulp.task('publish', function () {
+  console.log('TODO')
 })
--- index.js
+++ index.js
@@ -1,59 +1,73 @@
-var uuid = require('node-uuid')
+var JsonldParser = require('rdf-parser-jsonld')
+var parser = new JsonldParser()
 
+var context = {
+  '@vocab': 'http://schema.org/',
+  'id': '@id',
+  'type': '@type',
+  'ldp': 'http://www.w3.org/ns/ldp#',
+  'hydra': 'http://www.w3.org/ns/hydra/core#',
+  'flow': 'http://www.w3.org/2005/01/wf/flow#',
+  'foaf': 'http://xmlns.com/foaf/0.1/',
+  'resource': 'ldp:membershipResource',
+  'icr': 'ldp:insertedContentRelation',
+  'rel': 'ldp:hasMemberRelation',
+  'rev': 'ldp:isMemberOfRelation',
+  'primaryTopic': 'foaf:primaryTopic',
+  'contains': 'ldp:contains'
+}
 var Resource = function () {
-  this['@context'] = {
-    '@vocab': 'http://schema.org/',
-    'id': '@id',
-    'type': '@type',
-    'ldp': 'http://www.w3.org/ns/ldp#',
-    'flow': 'http://www.w3.org/2005/01/wf/flow#',
-    'resource': 'ldp:membershipResource',
-    'rel': 'ldp:hasMemberRelation',
-    'rev': 'ldp:isMemberOfRelation'
-  }
+  this['@context'] = context
   this['@graph'] = []
 }
 
-var repo = function (data, uriSpace) {
-  var uri = uriSpace + uuid.v4() + '#id'
+var repo = function (data, uri) {
   var doc = new Resource()
 
-  var sourceCode = {
+  var resource = {
     id: uri,
+    type: [ 'ldp:Resource', 'hydra:Resource' ]
+  }
+  doc['@graph'].push(resource)
+
+  var creation = {
+    id: resource.id + '#creation',
     type: [ 'CreativeWork', 'SoftwareSourceCode' ],
     name: data.name,
     description: data.description
   }
-  doc['@graph'].push(sourceCode)
+  doc['@graph'].push(creation)
 
-  var tasks = {
-    id: uriSpace + uuid.v4(),
-    type: 'ldp:IndirectContainer',
-    resource: sourceCode.id,
-    rel: 'flow:task'
-  }
-  doc['@graph'].push(tasks)
+  resource.primaryTopic = creation.id
 
-  return doc
+  return parser.parse(doc)
 }
 
-var task = function (data, uriSpace) {
-  var uri = uriSpace + uuid.v4() + '#id'
+var issue = function (data, uri) {
   var doc = new Resource()
 
-  var task = {
+  var resource = {
     id: uri,
+    type: [ 'ldp:Resource', 'hydra:Resource' ]
+  }
+  doc['@graph'].push(resource)
+
+  var task = {
+    id: resource.id + '#task',
     type: 'flow:Task',
     name: data.title,
     description: data.body
   }
   doc['@graph'].push(task)
 
-  return doc
+  resource.primaryTopic = task.id
+
+  return parser.parse(doc)
 }
 
 module.exports = {
   Resource: Resource,
+  context: context,
   repo: repo,
-  task: task
+  issue: issue
 }
